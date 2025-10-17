f5bdad533f60eb628b76c132d0f9921aa264876c
ProgramaConPid <programaconpid@gmail.com>
2025-10-17 06:43:12 -0500

    Remove dotenv from socket client

--- lib/socketClient.ts
+++ lib/socketClient.ts
@@ -1,8 +1,9 @@
 'use client';
-import dotenv from "dotenv"
-dotenv.config();
-
 import {io} from "socket.io-client";
-export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3000", {
+
+const socketUrl =
+  process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3000";
+
+export const socket = io(socketUrl, {
   transports: ['websocket'],
 });
